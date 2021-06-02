130d28dffb5522d024b85b55a268446efe9bfd08
Christoph Bock <christoph.bock@fau.de>
2021-06-02 10:43:18 +0200

    black linting

--- tests/test_json.py
+++ tests/test_json.py
@@ -4,13 +4,14 @@ import json
 import fauvqe.json
 from fauvqe import ADAM
 
+
 @pytest.mark.parametrize(
     "c",
     [
         [0],
         [1.1],
         [1.1j],
-        [1+1j],
+        [1 + 1j],
     ],
 )
 def test_complex(c):
@@ -20,22 +21,26 @@ def test_complex(c):
 
 
 def test_restore_error_unknown():
-    s = json.dumps({
-        fauvqe.json.JSONEncoder.FLAG: fauvqe.json.JSONEncoder.FLAG_RESTORABLE,
-        fauvqe.json.JSONEncoder.RESTORABLE_TYPE: 'I_DO_NOT_EXIST',
-        fauvqe.json.JSONEncoder.RESTORABLE_DATA: {},
-    })
+    s = json.dumps(
+        {
+            fauvqe.json.JSONEncoder.FLAG: fauvqe.json.JSONEncoder.FLAG_RESTORABLE,
+            fauvqe.json.JSONEncoder.RESTORABLE_TYPE: "I_DO_NOT_EXIST",
+            fauvqe.json.JSONEncoder.RESTORABLE_DATA: {},
+        }
+    )
 
     with pytest.raises(NotImplementedError):
         fauvqe.json.loads(s)
 
 
 def test_optimiser():
-    s = json.dumps({
-        fauvqe.json.JSONEncoder.FLAG: fauvqe.json.JSONEncoder.FLAG_RESTORABLE,
-        fauvqe.json.JSONEncoder.RESTORABLE_TYPE: 'ADAM',
-        fauvqe.json.JSONEncoder.RESTORABLE_DATA: {'constructor_params': {}},
-    })
+    s = json.dumps(
+        {
+            fauvqe.json.JSONEncoder.FLAG: fauvqe.json.JSONEncoder.FLAG_RESTORABLE,
+            fauvqe.json.JSONEncoder.RESTORABLE_TYPE: "ADAM",
+            fauvqe.json.JSONEncoder.RESTORABLE_DATA: {"constructor_params": {}},
+        }
+    )
     adam = fauvqe.json.loads(s)
 
     assert isinstance(adam, ADAM)
