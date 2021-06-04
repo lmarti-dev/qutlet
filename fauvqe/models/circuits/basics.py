4d68240f01af5a5325f7d19b705fe4e7af126494
Timo.Eckstein <Timo.Eckstein@fau.de>
2021-06-04 17:38:03 +0200

    Add mean Field initialisation to ising.basics

--- fauvqe/models/circuits/basics.py
+++ fauvqe/models/circuits/basics.py
@@ -45,6 +45,64 @@ def _neel_layer(self):
             if (qubit._row + qubit._col)%2 == 0:
                 yield cirq.X(qubit)
 
+
+def _mf_layer(self):
+    for row in self.qubits:
+        for qubit in row:
+            theta = self.basics.__get_mf_angle(self, qubit)
+            print("theta: \t{}".format(theta))
+            yield cirq.XPowGate(exponent=(theta/np.pi)).on(qubit)
+
+def __get_mf_angle(self, qubit):
+    #1. Calculate mean J
+    #        (1)
+    #         |
+    #   (2) - x - (3)
+    #         |
+    #        (4)
+    #
+    J_mean = 0
+    n_J = 0
+    
+    if (self.j_v.shape[0] * self.j_v.shape[1]) > 0:
+        # (1)
+        if qubit._row > 0:
+            J_mean += self.j_v[qubit._row - 1,  qubit._col]
+            n_J += 1
+        
+        # (4)
+        if qubit._col <  self.j_v.shape[1] and qubit._row <  self.j_v.shape[0]:
+            J_mean += self.j_v[qubit._row, qubit._col]
+            n_J += 1
+    
+    if (self.j_h.shape[0] * self.j_h.shape[1]) > 0: 
+        # (2)
+        if qubit._col > 0:
+            J_mean += self.j_h[qubit._row, qubit._col -1]
+            n_J += 1
+
+        # (3)
+        if qubit._col <  self.j_h.shape[1] and qubit._row <  self.j_h.shape[0]:
+            J_mean += self.j_h[qubit._row, qubit._col]
+            n_J += 1
+
+    # Get and return theta via
+    #sin theta = h_x/J_mean for spins
+    # need to map to qubit
+    # Hence use theta = (-1)^(col+row)*arccos(1- (h_ij/J(0))^2)^0.5
+    J_mean /= n_J
+    print("_col: \t{}, _row\t{}".format(qubit._col , qubit._row))
+    print("n_J: \t{}, \nJ_mean \t {}".format(n_J, J_mean))
+    print("h_ij: \t{}".format(self.h[qubit._row, qubit._col]))
+    print("self.j_v.shape \t {}".format(self.j_v.shape))
+    print("self.j_h.shape \t {}".format(self.j_h.shape))
+
+    if abs(self.h[qubit._row, qubit._col]) > abs(J_mean):
+        return np.pi/2
+    else:
+        return (-1)**(qubit._col+qubit._row)*\
+            np.arccos(np.sqrt(1-(self.h[qubit._row, qubit._col]/J_mean)**2))
+
 def set_circuit(self):
     if not self.basics.options['append']:
         self.circuit = cirq.Circuit()
@@ -52,7 +110,7 @@ def set_circuit(self):
     if self.basics.options["start"] == "neel":
         self.circuit.insert(0,self.basics._neel_layer(self))
     elif self.basics.options["start"] == "mf":
-
+        self.circuit.insert(0,self.basics._mf_layer(self))
     else:
-        assert (False), "Invalid hea parametrisation option, received: '{}', allowed is \n \
-                'joint', 'layerwise' and 'individual'".format(self.hea.options['parametrisation'] )
+        assert (False), "Invalid self.basics option, received: '{}', allowed is \n \
+                'neel',  and 'mf'".format(self.hea.options['parametrisation'] )
