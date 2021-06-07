17e9c0d3f0adf142e53d44f8bbd8f0280a4d1c5a
Timo.Eckstein <Timo.Eckstein@fau.de>
2021-06-07 19:46:44 +0200

    Remove prints in ising.basics

--- fauvqe/models/circuits/basics.py
+++ fauvqe/models/circuits/basics.py
@@ -10,19 +10,24 @@ import numpy as np
 import cirq
 import sympy
 
+def _hadamard_layer(self):
+    for row in self.qubits:
+        for qubit in row:
+            yield cirq.H.on(qubit)
+
 def rm_unused_cpv(self):
-    print(self.circuit_param)
+    #print(self.circuit_param)
     _erase_vec = self.circuit_param.copy()
     
     #1. Find all circuit parameter|variables that are used
     for moment in self.circuit.moments:
         #print(moment.__dict__)
         for operation in moment._operations:
-            print(operation._gate.__dict__.values())
+            #print(operation._gate.__dict__.values())
             symbols = list(operation._gate.__dict__.values())
             for element in symbols:
                 try:
-                    print(element.atoms(sympy.Symbol))
+                    #print(element.atoms(sympy.Symbol))
                     symbol = element.atoms(sympy.Symbol)
                     _erase_vec=set(_erase_vec) - symbol
                 except:
@@ -50,7 +55,7 @@ def _mf_layer(self):
     for row in self.qubits:
         for qubit in row:
             theta = self.basics.__get_mf_angle(self, qubit)
-            print("theta: \t{}".format(theta))
+            #print("theta: \t{}".format(theta))
             yield cirq.XPowGate(exponent=(theta/np.pi)).on(qubit)
 
 def __get_mf_angle(self, qubit):
@@ -91,11 +96,11 @@ def __get_mf_angle(self, qubit):
     # need to map to qubit
     # Hence use theta = (-1)^(col+row)*arccos(1- (h_ij/J(0))^2)^0.5
     J_mean /= n_J
-    print("_col: \t{}, _row\t{}".format(qubit._col , qubit._row))
-    print("n_J: \t{}, \nJ_mean \t {}".format(n_J, J_mean))
-    print("h_ij: \t{}".format(self.h[qubit._row, qubit._col]))
-    print("self.j_v.shape \t {}".format(self.j_v.shape))
-    print("self.j_h.shape \t {}".format(self.j_h.shape))
+    #print("_col: \t{}, _row\t{}".format(qubit._col , qubit._row))
+    #print("n_J: \t{}, \nJ_mean \t {}".format(n_J, J_mean))
+    #print("h_ij: \t{}".format(self.h[qubit._row, qubit._col]))
+    #print("self.j_v.shape \t {}".format(self.j_v.shape))
+    #print("self.j_h.shape \t {}".format(self.j_h.shape))
 
     if abs(self.h[qubit._row, qubit._col]) > abs(J_mean):
         return np.pi/2
@@ -107,10 +112,12 @@ def set_circuit(self):
     if not self.basics.options['append']:
         self.circuit = cirq.Circuit()
 
-    if self.basics.options["start"] == "neel":
+    if self.basics.options["start"] == "hadamard":
+        self.circuit.insert(0,self.basics._hadamard_layer(self))
+    elif self.basics.options["start"] == "neel":
         self.circuit.insert(0,self.basics._neel_layer(self))
     elif self.basics.options["start"] == "mf":
         self.circuit.insert(0,self.basics._mf_layer(self))
     else:
         assert (False), "Invalid self.basics option, received: '{}', allowed is \n \
-                'neel',  and 'mf'".format(self.hea.options['parametrisation'] )
+                'hadamard', 'neel',  and 'mf'".format(self.hea.options['parametrisation'] )
