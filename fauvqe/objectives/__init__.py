5fd768bf8f84448300a4bdd85b17748f508003a4
Refik <refik.mansuroglu@fau.de>
2021-07-23 14:12:23 +0200

    Removed odd fidelity.py from dev-hea-time branch

--- fauvqe/objectives/__init__.py
+++ fauvqe/objectives/__init__.py
@@ -6,4 +6,3 @@ from fauvqe.objectives.objective import Objective
 from fauvqe.objectives.expectationvalue import ExpectationValue
 from fauvqe.objectives.cvar import CVaR
 from fauvqe.objectives.utcost import UtCost
-from fauvqe.objectives.fidelity import Fidelity
