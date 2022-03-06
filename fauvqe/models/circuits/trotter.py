6da221c7a48570b83f35acc6b2a95c9210b2d2e3
Refik <refik.mansuroglu@fau.de>
2022-03-06 01:40:52 +0100

    Added Docstrings, comments and typing for trotter.py

--- fauvqe/models/circuits/trotter.py
+++ fauvqe/models/circuits/trotter.py
@@ -1,42 +1,104 @@
 """
-    Include here methods that help generating Trotterized time evolutions
+This is a submodule for instances of AbstractModel and for the UtCost objective
+Include here methods that help generating Trotterized time evolutions
 """
 from fauvqe.models.abstractmodel import AbstractModel
 
 # external import
 import numpy as np
 import cirq
+from numbers import Real
 import sympy
 
 def set_circuit(self):
+    """
+        Sets the circuit for Trotter-Suzuki sequence of order q and with Trotter number m
+        
+        Parameters
+        ----------
+        self
+        
+        Returns
+        -------
+        void
+    """
+    #assert Trotter order and Trotter number are Integers
     assert isinstance(self.trotter.options['q'], int), "Trotter order q must be Integer. Received: {}".format(q)
     assert isinstance(self.trotter.options['m'], int), "Trotter number m must be Integer. Received: {}".format(m)
+    #Reset circuit
     if not self.trotter.options['append']:
         self.circuit = cirq.Circuit()
+    #Call helper function that generates a circuit from the Hamiltonian PauliSum
     self.circuit.append(self.trotter.get_trotter_circuit_from_hamiltonian(self, self.hamiltonian, self.t, self.trotter.options['q'], self.trotter.options['m']))
 
-def get_trotter_circuit_from_hamiltonian(self, hamiltonian, t, q, m):
+def get_trotter_circuit_from_hamiltonian(self, hamiltonian: cirq.PauliSum, t: Real, q: np.uint, m: np.uint) -> cirq.Circuit:
+    """
+        Sets the circuit for Trotter-Suzuki sequence of order q and with Trotter number m
+        
+        Parameters
+        ----------
+        self
+        hamiltonian: cirq.PauliSum, 
+            Hamiltonian that is trotterized.
+            We iterate over the addends which are tuples of PauliStrings together with their interaction coefficients
+        t: Real
+            Simulation time
+        q: np.uint
+            Order of Approximation
+        m: np.uint
+            Refinement of time R. t -> ( t/R )^R
+        
+        Returns
+        -------
+        q=1: (\prod_k e^(-i t/m h_k) )^m: cirq.Circuit
+        q=2: symmetrized
+        q=2k: see https://arxiv.org/abs/1912.08854
+    """
     return m * self.trotter.get_single_step_trotter_circuit_from_hamiltonian(self, hamiltonian, t/m, q)
     
-def get_single_step_trotter_circuit_from_hamiltonian(self, hamiltonian, t, q):
+def get_single_step_trotter_circuit_from_hamiltonian(self, hamiltonian: cirq.PauliSum, t: Real, q: np.uint):
+    """
+        Sets the single time step t/m for Trotter-Suzuki sequence of order q. Different cases for order q:
+        q=1: Linear Trotter-Suzuki
+        q=2: Symmetrized gate ordering
+        q=2k: Higher orderings, see https://arxiv.org/abs/1912.08854
+        
+        Parameters
+        ----------
+        self
+        hamiltonian: cirq.PauliSum, 
+            Hamiltonian that is trotterized.
+            We iterate over the addends which are tuples of PauliStrings together with their interaction coefficients
+        t: Real
+            Simulation time
+        q: np.uint
+            Order of Approximation
+        
+        Returns
+        -------
+        q=1: \prod_k e^(-i t/m h_k): cirq.Circuit
+    """
     if(q == 1):
         return self.trotter._first_order_trotter_circuit(self, hamiltonian, t)
     elif(q == 2):
+        #forward order
         half = self.trotter._first_order_trotter_circuit(self, hamiltonian, 0.5*t)
+        #backward order
         for k in range(len(half)-1, -1, -1):
             half.append(half[k])
         return half
     elif( (q % 2) == 0):
         nu = 1/(4 - 4**(1/(q - 1)))
+        #recursive relation
         partone = self.trotter.get_single_step_trotter_circuit_from_hamiltonian(self, hamiltonian, nu*t, q-2)
         parttwo = self.trotter.get_single_step_trotter_circuit_from_hamiltonian(self, hamiltonian, (1-4*nu)*t, q-2)
         return 2*partone + parttwo + 2*partone
     else:
         raise NotImplementedError()
     
-def _first_order_trotter_circuit(self, hamiltonian, t):
+def _first_order_trotter_circuit(self, hamiltonian: cirq.PauliSum, t: Real) -> cirq.Circuit:
     """
-    This function initialises the circuit for Trotter approximation.
+    This function initialises the circuit for q=1 Trotter approximation.
     
     Parameters
     ----------
@@ -44,10 +106,6 @@ def _first_order_trotter_circuit(self, hamiltonian, t):
         System Hamiltonian
     t: float
         Total simulation time
-    q: np.uint
-        Order of Approximation
-    m: np.uint
-        Refinement of time R. t -> ( t/R )^R
     
     Returns
     ---------
