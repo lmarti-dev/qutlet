a0ff80164a5efc9b24c513aba73b8d18b65482ca
Refik <refik.mansuroglu@fau.de>
2022-06-10 14:17:13 +0200

    Main tests written, still not all pass

--- tests/test_cooled_adiabatic.py
+++ tests/test_cooled_adiabatic.py
@@ -6,7 +6,7 @@ from scipy.linalg import expm
 import sympy
 
 # internal imports
-from fauvqe import CooledAdiabatic, Adiabatic, Ising, Heisenberg, HeisenbergFC, ExpectationValue
+from fauvqe import CooledAdiabatic, Ising, Heisenberg, HeisenbergFC, ExpectationValue
 
 def test__eq__():
     n = [1,3]; boundaries = [1, 0]
@@ -20,7 +20,8 @@ def test__eq__():
                     np.ones((n[0], n[1]-boundaries[1])), 
                        np.ones((n[0], n[1])),
                        np.ones((n[0], n[1])),
-                       np.ones((n[0], n[1])))
+                       np.ones((n[0], n[1]))
+                   )
     
     H1 = Ising("GridQubit", 
                n, 
@@ -123,7 +124,11 @@ def test_copy(qubittype, n, j_x_v, j_x_h, j_y_v, j_y_h, j_z_v, j_z_h, h_x, h_y, 
     H0 = Heisenberg(qubittype, n, j_x_v, j_x_h, j_y_v, j_y_h, j_z_v, j_z_h, h_x, h_y, h_z)
     H1 = Ising(qubittype, n, j_z_v, j_z_h, h_x, "X")
     
-    model = Adiabatic(H0, H1)
+    m_anc = Ising("GridQubit", [1,n[1]], np.zeros((1,n[1])), np.zeros((1,n[1])), np.ones((1,n[1])))
+    j_int = np.ones((1, *n))
+    int_gates = [lambda q1, q2: cirq.X(q1)*cirq.X(q2)]
+    
+    model = CooledAdiabatic(H0, H1, m_anc, int_gates, j_int)
     model.set_circuit("qaoa")
     model2 = model.copy()
     
@@ -134,15 +139,20 @@ def test_copy(qubittype, n, j_x_v, j_x_h, j_y_v, j_y_h, j_z_v, j_z_h, h_x, h_y, 
     assert( model is not model2 )
 
 def test_json():
+    n = [1, 2]
     H0 = Heisenberg("GridQubit", [1, 2], np.zeros((0, 2)), np.ones((1, 1)), np.zeros((0, 2)), np.ones((1, 1)), np.zeros((0, 2)), np.ones((1, 1)), np.ones((1, 2)), np.ones((1, 2)), np.ones((1, 2)))
     
     H1 = Heisenberg("GridQubit", [1, 2], np.zeros((0, 2)), np.ones((1, 1)), np.zeros((0, 2)), np.ones((1, 1)), np.zeros((0, 2)), np.ones((1, 1)), np.ones((1, 2)), np.ones((1, 2)), np.ones((1, 2)))
     
-    model = Adiabatic(H0, H1)
+    m_anc = Ising("GridQubit", [1,n[1]], np.zeros((1,n[1])), np.zeros((1,n[1])), np.ones((1,n[1])))
+    j_int = np.ones((1, *n))
+    int_gates = [lambda q1, q2: cirq.X(q1)*cirq.X(q2)]
+    
+    model = CooledAdiabatic(H0, H1, m_anc, int_gates, j_int)
     
     json = model.to_json_dict()
     
-    model2 = Adiabatic.from_json_dict(json)
+    model2 = CooledAdiabatic.from_json_dict(json)
     
     assert (model == model2)
 
@@ -208,16 +218,27 @@ def test_set_hamiltonian_override(qubittype, n, j_v, j_h, h, t, field, sol):
     zeros_v = np.zeros((n[0]-1, n[1]))
     zeros_h = np.zeros((n[0], n[1]-1))
     zeros = np.zeros((n[0], n[1]))
+    N = 2**(n[0]*n[1])
+    X = np.array([
+        [0, 1],
+        [1, 0]
+    ])
+    
     H0 = Ising(qubittype, n, j_v, j_h, h, field)
     if(field == 'X'):
         H1 = Heisenberg(qubittype, n, j_v, j_h, zeros_v, zeros_h, zeros_v, zeros_h, zeros, zeros, zeros)
     else:
         H1 = Heisenberg(qubittype, n, zeros_v, zeros_h, zeros_v, zeros_h, j_v, j_h, zeros, zeros, zeros)
     
-    model = Adiabatic(H0, H1, t=t)
-        
-    print(model.hamiltonian.matrix())
-    assert np.linalg.norm(model.hamiltonian.matrix() - sol) < 1e-13
+    m_anc = Ising("GridQubit", [1,n[1]], np.zeros((1,n[1])), np.zeros((1,n[1])), np.ones((1,n[1])), 'Z')
+    j_int = np.ones((1, *n))
+    int_gates = [lambda q1, q2: cirq.X(q1)*cirq.X(q2)]
+    model = CooledAdiabatic(H0, H1, m_anc, int_gates, j_int, t=t)
+    
+    solution = np.kron(sol, np.eye(N)) + np.kron(np.eye(N), m_anc.hamiltonian.matrix()) - np.kron(np.eye(2), np.kron(X, np.kron(np.eye(2), X))) - np.kron(X, np.kron(np.eye(2), np.kron(X, np.eye(2))))
+    print(solution - (model.hamiltonian.matrix()))
+    print(model.hamiltonian)
+    assert np.linalg.norm(model.hamiltonian.matrix() - solution ) < 1e-13
 
 
 @pytest.mark.parametrize(
@@ -253,9 +274,13 @@ def test_set_uts(qubittype, n, j_v, j_h, h, T, field):
     else:
         H1 = Heisenberg(qubittype, n, zeros_v, zeros_h, zeros_v, zeros_h, j_v, j_h, zeros, zeros, h)
     
-    model = Adiabatic(H0, H1, T=T)
+    m_anc = Ising("GridQubit", [1,n[1]], np.zeros((1,n[1])), np.zeros((1,n[1])), np.ones((1,n[1])), 'Z')
+    j_int = np.ones((1, *n))
+    int_gates = [lambda q1, q2: cirq.X(q1)*cirq.X(q2)]
+    model = CooledAdiabatic(H0, H1, m_anc, int_gates, j_int, T=T)
     model.set_Uts()
-    N=2**(n[0]*n[1])
+    
+    N=2**(2*n[0]*n[1])
     res = np.eye(N)
     for i in range(len(model._Uts)):
         res = model._Uts[i] @ res
@@ -266,113 +291,3 @@ def test_set_uts(qubittype, n, j_v, j_h, h, T, field):
     print(out)
     print(H1.eig_vec.transpose()[0])
     assert 1-abs((H1.eig_vec.transpose()[0]).transpose().conjugate() @ out ) < 1e-3
-
-
-@pytest.mark.parametrize(
-    "qubittype, n, j_v, j_h, h, T, field",
-    [
-        (
-            "GridQubit",
-            [1, 2],
-            np.ones((0, 2)),
-            np.ones((1, 1)),
-            np.ones((1, 2)),
-            100,
-            'X'
-        ),
-        (
-            "GridQubit",
-            [1, 2],
-            np.ones((0, 2)),
-            np.ones((1, 1)),
-            np.ones((1, 2)),
-            100,
-            'Z'
-        )
-    ]
-)
-def test_set_ut_custom_sweep(qubittype, n, j_v, j_h, h, T, field):
-    zeros_v = np.zeros((n[0]-1, n[1]))
-    zeros_h = np.zeros((n[0], n[1]-1))
-    zeros = np.zeros((n[0], n[1]))
-    H0 = Ising(qubittype, n, j_v, j_h, h, field)
-    if(field == 'X'):
-        H1 = Heisenberg(qubittype, n, j_v, j_h, zeros_v, zeros_h, zeros_v, zeros_h, zeros, h, zeros)
-    else:
-        H1 = Heisenberg(qubittype, n, zeros_v, zeros_h, zeros_v, zeros_h, j_v, j_h, zeros, zeros, zeros)
-    
-    for sweep in [lambda time: time**2/T**2, lambda time: 4*(time/T - 0.5)**3 + 0.5]:
-        model = Adiabatic(H0, H1, sweep, T=T)
-        model.set_Uts()
-        N=2**(n[0]*n[1])
-        res = np.eye(N)
-        for i in range(len(model._Uts)):
-            res = model._Uts[i] @ res
-        H0.diagonalise(solver="numpy")
-        initial = H0.eig_vec.transpose()[0]
-        out = res @ initial
-        H1.diagonalise(solver="numpy")
-        print(out)
-        print(H1.eig_vec.transpose()[0])
-        assert 1-abs((H1.eig_vec.transpose()[0]).transpose().conjugate() @ out ) < 1e-2
-
-@pytest.mark.parametrize(
-    "qubittype, n, j_v, j_h, h, t, field, sol",
-    [
-        (
-            "GridQubit",
-            [1, 2],
-            np.ones((0, 2)),
-            np.ones((1, 1)),
-            np.ones((1, 2)),
-            0,
-            'X',
-            -0.5
-        ),
-        (
-            "GridQubit",
-            [1, 2],
-            np.ones((0, 2)),
-            np.ones((1, 1)),
-            np.ones((1, 2)),
-            1,
-            'X',
-            0.0
-        ),
-        (
-            "GridQubit",
-            [1, 2],
-            np.ones((0, 2)),
-            np.ones((1, 1)),
-            np.ones((1, 2)),
-            0.5,
-            'X',
-            -0.25
-        ),
-        (
-            "GridQubit",
-            [1, 2],
-            np.ones((0, 2)),
-            np.ones((1, 1)),
-            np.ones((1, 2)),
-            0.5,
-            'Z',
-            -1
-        )
-    ]
-)
-def test_energy(qubittype, n, j_v, j_h, h, t, field, sol):
-    zeros_v = np.zeros((n[0]-1, n[1]))
-    zeros_h = np.zeros((n[0], n[1]-1))
-    zeros = np.zeros((n[0], n[1]))
-    H0 = Ising(qubittype, n, j_v, j_h, h, field)
-    if(field == 'X'):
-        H1 = Heisenberg(qubittype, n, j_v, j_h, zeros_v, zeros_h, zeros_v, zeros_h, zeros, zeros, zeros)
-    else:
-        H1 = Heisenberg(qubittype, n, zeros_v, zeros_h, zeros_v, zeros_h, j_v, j_h, zeros, zeros, zeros)
-    
-    model = Adiabatic(H0, H1, t=t)
-    obj = ExpectationValue(model)
-    ini = np.zeros(2**(n[0]*n[1])).astype(np.complex64)
-    ini[0] = 1
-    assert abs(obj.evaluate(ini) - sol) < 1e-13
