0deba12b2e64d3288483778c34c7ce3cb6d0677f
Timo.Eckstein <Timo.Eckstein@fau.de>
2022-07-26 01:35:17 +0200

    100% test coverage of ANNNI

--- tests/test_models_annni.py
+++ tests/test_models_annni.py
@@ -95,10 +95,79 @@ from fauvqe import ANNNI, Converter, Ising
             cirq.PauliSum.from_pauli_strings([-cirq.Z(cirq.GridQubit(j,i))*cirq.Z(cirq.GridQubit(j,i+1))  for i in range(2-1) for j in range(2)]) +
             cirq.PauliSum.from_pauli_strings([-0.8*cirq.X(cirq.GridQubit(j,i)) for i in range(2) for j in range(2)]),
         ),
+        (
+            [2,2],
+            1,
+            0.5,
+            0.8*np.ones((1, 2,2)),
+            1,
+            cirq.PauliSum.from_pauli_strings([-cirq.Z(cirq.GridQubit(j,i))*cirq.Z(cirq.GridQubit(j+1,i)) for i in range(2) for j in range(2-1)]) +
+            cirq.PauliSum.from_pauli_strings([-cirq.Z(cirq.GridQubit(j,i))*cirq.Z(cirq.GridQubit(j,i+1))  for i in range(2-1) for j in range(2)]) +
+            cirq.PauliSum.from_pauli_strings([-0.8*cirq.X(cirq.GridQubit(j,i)) for i in range(2) for j in range(2)]),
+        ),
+        (
+            [2,3],
+            1,
+            0.5,
+            0.8*np.ones((1, 2,3)),
+            1,
+            cirq.PauliSum.from_pauli_strings([-cirq.Z(cirq.GridQubit(j,i))*cirq.Z(cirq.GridQubit(j+1,i)) for i in range(3) for j in range(2-1)]) +
+            cirq.PauliSum.from_pauli_strings([-cirq.Z(cirq.GridQubit(j,i))*cirq.Z(cirq.GridQubit(j,i+1))  for i in range(3-1) for j in range(2)]) +
+            cirq.PauliSum.from_pauli_strings([-0.5*cirq.Z(cirq.GridQubit(j,i))*cirq.Z(cirq.GridQubit(j,i+2))  for i in range(3-2) for j in range(2)]) +
+            cirq.PauliSum.from_pauli_strings([-0.8*cirq.X(cirq.GridQubit(j,i)) for i in range(3) for j in range(2)]),
+        ),
     ]
 )
 def test_constructor_hamiltonian(n, J, k, h, boundaries, hamiltonian):
     annni_obj = ANNNI(n, J, k, h, boundaries)
+
+    assert annni_obj.qubittype == "GridQubit"
+    assert len(annni_obj.n) == 2
+    print("hamiltonian:\n{}\n".format(hamiltonian))
+    print("annni_obj._hamiltonian:\n{}\n".format(annni_obj._hamiltonian))
+    print("hamiltonian-annni_obj._hamiltonian:\n{}\n".format(hamiltonian-annni_obj._hamiltonian))
+    assert annni_obj._hamiltonian == hamiltonian
+
+@pytest.mark.parametrize(
+    "n, J, k, h, boundaries, hamiltonian",
+    [
+       (
+            np.array([2,3]),
+            0.7,
+            0.5,
+            0.8*np.ones((1, 2,3)),
+            1,
+            cirq.PauliSum.from_pauli_strings([-0.7*cirq.Z(cirq.GridQubit(j,i))*cirq.Z(cirq.GridQubit(j+1,i)) for i in range(3) for j in range(2-1)]) +
+            cirq.PauliSum.from_pauli_strings([-0.7*cirq.Z(cirq.GridQubit(j,i))*cirq.Z(cirq.GridQubit(j,i+1))  for i in range(3-1) for j in range(2)]) +
+            cirq.PauliSum.from_pauli_strings([-0.5*cirq.Z(cirq.GridQubit(j,i))*cirq.Z(cirq.GridQubit(j,i+2))  for i in range(3-2) for j in range(2)]) +
+            cirq.PauliSum.from_pauli_strings([-0.8*cirq.X(cirq.GridQubit(j,i)) for i in range(3) for j in range(2)]),
+        ),
+    ]
+)
+def test_constructor_hamiltonian2(n, J, k, h, boundaries, hamiltonian):
+    _J_array = np.zeros((1, *n, *n))
+    for n0 in range(n[0]):
+        for n1 in range(n[1]-1):
+            _J_array[0,n0,n1+1,n0,n1] = J
+            _J_array[0,n0,n1,n0,n1+1] = J
+
+    for n0 in range(n[0]-1):
+        for n1 in range(n[1]):
+            _J_array[0,n0+1,n1,n0,n1] = J
+            _J_array[0,n0,n1,n0+1,n1] = J
+
+    _k_array = np.zeros((1, *n, *n))
+    for n0 in range(n[0]):
+        for n1 in range(n[1]-2):
+            _k_array[0,n0,n1+2,n0,n1] = k
+            _k_array[0,n0,n1,n0,n1+2] = k
+
+    for n0 in range(n[0]-2):
+        for n1 in range(n[1]):
+            _k_array[0,n0+2,n1,n0,n1] = k
+            _k_array[0,n0,n1,n0+2,n1] = k
+
+    annni_obj = ANNNI(n, _J_array, _k_array, h, boundaries)
 
     assert annni_obj.qubittype == "GridQubit"
     assert len(annni_obj.n) == 2
