cfbb86d6a8f422510680911c0413267cfa7c6b75
TimoEckstein <Timo.Eckstein@fau.de>
2022-11-18 12:20:38 +0100

    Add tests for new trotter implementation and clean-up trotter circuit file

--- fauvqe/circuits/trotter.py
+++ fauvqe/circuits/trotter.py
@@ -123,61 +123,11 @@ def _first_order_trotter_circuit(   self,
     
     Returns
     ---------
-    res: cirq.Circuit()
         Circuit describing Trotterized Time Evolution
     """
-    #res = cirq.Circuit()
-    #Loop through all the addends in the PauliSum Hamiltonian
-    #for pstr in hamiltonian._linear_dict:
-    #temp encodes each of the PauliStrings in the PauliSum hamiltonian which need to be turned into gates
-    #temp = 1
-    #Loop through Paulis in the PauliString (pauli[1] encodes the cirq gate and pauli[0] encodes the qubit on which the gate acts)
-    #for pauli in pstr:
-    #    temp = temp * pauli[1](pauli[0])
-    #   Append the PauliString gate in temp to the power of the time step * coefficient of said PauliString. 
-    # The coefficient needs to be multiplied by a correction factor of 2/pi in order for the PowerGate to represent a Pauli exponential.
-
-    #gate = self.trotter._get_gate_from_paulistring(self, pstr)
-    #res.append(temp**np.real(2/np.pi * float(t) * float(np.real(hamiltonian._linear_dict[pstr]))))
-    #res.append(self.trotter._get_gate_from_paulistring2(self, pstr, hamiltonian, t))
-    #Copy the Trotter layer *m times.
-    #return res
-    
-    #return cirq.Circuit(*[self.trotter._get_gate_from_paulistring(self, pstr)**np.real(2/np.pi * float(t) * float(np.real(hamiltonian._linear_dict[pstr]))) for pstr in hamiltonian._linear_dict])
-    return cirq.Circuit(*[self.trotter._get_gate_from_paulistring2(self, pstr, hamiltonian, t) for pstr in hamiltonian._linear_dict])
+    return cirq.Circuit(*[self.trotter._get_gate_from_paulistring(self, pstr, hamiltonian, t) for pstr in hamiltonian._linear_dict])
 
 def _get_gate_from_paulistring( self,
-                                pauli_string: frozenset):
-    #Try to just return appropiate gates
-    _paulis=[]
-    _qubits =[]
-    temp=1
-    for item in pauli_string:
-        temp = temp * item[1](item[0])
-        _qubits.append(item[0])
-        _paulis.append(item[1])
-    if len(_paulis) > 2:
-        return temp
-    elif len(_paulis) == 2:
-        if _paulis[0] != _paulis[1]:
-            return temp
-        elif _paulis[0] == cirq.Z:
-            return cirq.ZZ.on(*_qubits)
-        elif _paulis[0] == cirq.X:
-            return cirq.XX.on(*_qubits)
-        else:
-            return cirq.YY.on(*_qubits)
-    elif len(_paulis) == 1:
-        if _paulis[0] == cirq.X:
-            return cirq.X.on(*_qubits)
-        elif _paulis[0] == cirq.Z:
-            return cirq.Z.on(*_qubits)
-        else:
-            return cirq.Y.on(*_qubits)
-    else:
-        return cirq.IdentityGate
-
-def _get_gate_from_paulistring2( self,
                                 pauli_string: frozenset,
                                 hamiltonian: cirq.PauliSum, 
                                 t: Real):
--- tests/test_circuits_trotter.py
+++ tests/test_circuits_trotter.py
@@ -1,10 +1,12 @@
 # external imports
+import cirq
 import pytest
 import numpy as np
 import scipy
+from numbers import Real
 
 # internal imports
-from fauvqe import Ising, HeisenbergFC, UtCost
+from fauvqe import Ising, HeisenbergFC, MatrixCost, UtCost
 
 @pytest.mark.parametrize(
     "n, boundaries, field, options, t",
@@ -239,3 +241,132 @@ def test_get_params_errors(n, boundaries, J, h, options, t):
     ising.set_circuit("trotter", options)
     with pytest.raises(NotImplementedError):
         ising.trotter.get_parameters(ising)
+
+def _get_random_PauliSum(qubits, length, Isallpowgates):
+    assert length >= len(qubits)-1
+    #First do 2 qubit random paulis on all qubits
+    #Then add more PauliStrings to get PauliSum length
+    _SingleQubitGates=[cirq.X,cirq.Y,cirq.Z]
+    if Isallpowgates:
+        _TwoQubitGates=[[gate,gate] for gate in _SingleQubitGates]
+    else:     
+        _TwoQubitGates=[[gate1,gate2] for gate1 in _SingleQubitGates for gate2 in _SingleQubitGates]
+
+    _paulisum = cirq.PauliSum()
+    for i in range(len(qubits)-1):
+        _rnd_int = np.random.randint(len(_TwoQubitGates), size=1)[0]
+        _rnd_coefficent = float(2*np.random.rand(1)[0]-1)
+
+        print(_TwoQubitGates[_rnd_int][0].on(qubits[i]))
+        print(_TwoQubitGates[_rnd_int][1].on(qubits[i+1]) )
+        _paulisum += (_rnd_coefficent*_TwoQubitGates[_rnd_int][0].on(qubits[i])*_TwoQubitGates[_rnd_int][1].on(qubits[i+1]) )
+
+    for i in range(length+1-len(qubits)):
+        _rnd_coefficent = float(2*np.random.rand(1)[0]-1)
+        if float(2*np.random.rand(1)[0]-1) > 0:
+            _rnd_int_gate = np.random.randint(len(_TwoQubitGates), size=1)[0]
+            _rnd_int_qubit1 = np.random.randint(len(qubits), size=1)[0]
+            _rnd_int_qubit2 = np.random.randint(len(qubits), size=1)[0]
+            while _rnd_int_qubit1 == _rnd_int_qubit2:
+                _rnd_int_qubit2 = np.random.randint(len(qubits), size=1)[0]
+            _paulisum += (_rnd_coefficent   *_TwoQubitGates[_rnd_int_gate][0].on(qubits[_rnd_int_qubit1])\
+                                            *_TwoQubitGates[_rnd_int_gate][1].on(qubits[_rnd_int_qubit2]) )
+        else:
+            _rnd_int_gate = np.random.randint(len(_SingleQubitGates), size=1)[0]
+            _rnd_int_qubit = np.random.randint(len(qubits), size=1)[0]
+            _paulisum += (_rnd_coefficent*_SingleQubitGates[_rnd_int_gate].on(qubits[_rnd_int_qubit]) )
+
+    return _paulisum
+        
+def _previous_circuit(  hamiltonian: cirq.PauliSum, 
+                        t: Real):
+    res = cirq.Circuit()
+    #Loop through all the addends in the PauliSum Hamiltonian
+    for pstr in hamiltonian._linear_dict:
+        #temp encodes each of the PauliStrings in the PauliSum hamiltonian which need to be turned into gates
+        temp = 1
+        #Loop through Paulis in the PauliString (pauli[1] encodes the cirq gate and pauli[0] encodes the qubit on which the gate acts)
+        for pauli in pstr:
+            temp = temp * pauli[1](pauli[0])
+        #   Append the PauliString gate in temp to the power of the time step * coefficient of said PauliString. 
+        # The coefficient needs to be multiplied by a correction factor of 2/pi in order for the PowerGate to represent a Pauli exponential.
+
+        res.append(temp**np.real(2/np.pi * float(t) * float(np.real(hamiltonian._linear_dict[pstr]))))
+
+        #Copy the Trotter layer *m times.
+    return res
+
+@pytest.mark.parametrize(
+    "n, string_length, Isallpowgates",
+    [
+        (
+            2,
+            1,
+            True,      
+        ),
+        (
+             2,
+             5,
+             True,       
+        ),
+        (
+             2,
+             3,
+             False,        
+         ),
+         (
+            3,
+            2,
+            True,      
+        ),
+        (
+             3,
+             5,
+             True,       
+        ),
+        (
+             3,
+             3,
+             False,        
+         ),
+        (
+            4,
+            3,
+            True,       
+        ),
+        (
+            4,
+            5,
+            True,     
+        ),
+        (
+            4,
+            3,
+            False,       
+        ),
+    ]
+)
+def test_consistency_previous_circuit_implementation(n, string_length, Isallpowgates):
+    t = 1 + float(np.random.rand(1)[0])
+    qubits=cirq.LineQubit.range(n)
+
+    #get random PauliString on n qubits
+    #with/without non-PowGates
+    _paulisum = _get_random_PauliSum(qubits, string_length, Isallpowgates)
+
+    _old_circuit=_previous_circuit(_paulisum, t)
+
+    dummy_model = Ising(  "GridQubit", 
+                    [1,n], 
+                    np.ones((0, n)), 
+                    np.ones((1, n-1)), 
+                    np.ones((1, n)), 
+                    "X", 
+                    t=t)
+
+    _new_circuit=dummy_model.trotter._first_order_trotter_circuit(   dummy_model, _paulisum, t)
+
+    cost=MatrixCost(dummy_model, cirq.unitary(_new_circuit), exponent=2)
+
+    assert abs(cost.evaluate(cirq.unitary(_old_circuit))) < 1e-14
+    
