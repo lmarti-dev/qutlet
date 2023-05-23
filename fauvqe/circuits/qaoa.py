7d5cbcaa646a38fbc26b493bbaa8f35740107db7
TimoEckstein <Timo.Eckstein@fau.de>
2023-05-23 15:05:24 +0200

    Erase try/except in set_symbols of circuits.qaoa as AbstractModels declears already self.circuit_param

--- fauvqe/circuits/qaoa.py
+++ fauvqe/circuits/qaoa.py
@@ -7,79 +7,6 @@ from importlib import import_module
 import itertools
 import sympy
 
-def set_p(self, p):
-    if self.qaoa.options.get("p") != p:
-        self.qaoa.options["p"] = p
-        self.qaoa.options["append"] = False
-        # Need to reset circuit
-        self.qaoa.set_symbols(self)
-        self.qaoa.set_circuit(self)
-        # Erase beta_values, gamma_values as these are no fiting anymore
-        # rename to general circuit_param_values
-        # self.circuit_param_values is created in  self.qaoa.set_symbols(self, p)
-        # if it does not exist
-        
-        #In general, this does not make sense anymore as we only 
-        del self.circuit_param_values
-
-
-def set_symbols(self):
-    # This creats a circuit-param list like [b0, g0, b1, g1 ] etc
-    p = self.qaoa.options.get("p")
-    assert isinstance(p, (int, np.int_)), "Error: p needs to be int, received {}".format(type(p))
-    temp = []
-    for i in range(p):
-        temp.append(sympy.Symbol("b" + str(i)))
-        temp.append(sympy.Symbol("g" + str(i)))
-    
-    if not self.qaoa.options.get("append"):
-        self.circuit_param = temp
-    else:
-        self.qaoa.options['i0'] = np.size(self.circuit_param)
-        try:
-            self.circuit_param.extend(temp)
-        except:
-            self.circuit_param = temp
-
-
-def _set_beta_values(self, beta_values):
-    p = self.qaoa.options.get("p")
-    i0 = self.qaoa.options.get("i0")
-
-    assert np.size(beta_values) == p, "Error: size(beta_values) !=  p; {} != {}".format(
-            np.size(beta_values), p
-        )
-
-    assert np.size(self.circuit_param_values) == 2 * p +i0, " np.size(self.circuit_param_values) !=  2p + i0; {} != {}".format(
-            np.size(self.circuit_param_values) , 2 * p +i0
-        )
-
-    if p > 1:
-        for i in range(p):
-            self.circuit_param_values[i0 + 2 * i] = beta_values[i]
-    else:
-        self.circuit_param_values[i0 + 0] = beta_values
-
-
-def _set_gamma_values(self, gamma_values):
-    p = self.qaoa.options.get("p")
-    i0 = self.qaoa.options.get("i0")
-
-    assert np.size(gamma_values) == p, "Error: size(gamma values) !=  p; {} != {}".format(
-            np.size(gamma_values), p
-        )
-
-    assert np.size(self.circuit_param_values) == 2 * p +i0, " np.size(self.circuit_param_values) !=  2p + i0; {} != {}".format(
-            np.size(self.circuit_param_values) , 2 * p +i0
-        )
-
-    if p > 1:
-        for i in range(p):
-            self.circuit_param_values[i0 + 2 * i + 1] = gamma_values[i]
-    else:
-        self.circuit_param_values[i0 + 1] = gamma_values
-
-
 def set_circuit(self):
     p = self.qaoa.options.get("p") 
     # Reset circuit
@@ -102,15 +29,98 @@ def set_circuit(self):
         if hasattr(self, 'field') and self.field == "Z":
             self.circuit.append(cirq.Moment(self.qaoa._Z_layer(self, self.circuit_param[i0 + 2 * i + 1])))
         self.circuit.append(cirq.Moment(self.qaoa._UB_layer(self, self.circuit_param[i0 + 2 * i])))
+
+def set_p(self, p):
+    if self.qaoa.options.get("p") != p:
+        self.qaoa.options["p"] = p
+        self.qaoa.options["append"] = False
+        # Need to reset circuit
+        self.qaoa.set_symbols(self)
+        self.qaoa.set_circuit(self)
+        # Erase beta_values, gamma_values as these are no fiting anymore
+        # rename to general circuit_param_values
+        # self.circuit_param_values is created in  self.qaoa.set_symbols(self, p)
+        # if it does not exist
         
+        #In general, this does not make sense anymore as we only 
+        del self.circuit_param_values
 
+def set_symbols(self):
+    # This creats a circuit-param list like [b0, g0, b1, g1 ] etc
+    p = self.qaoa.options.get("p")
+    assert isinstance(p, (int, np.int_)), "Error: p needs to be int, received {}".format(type(p))
+    temp = []
+    for i in range(p):
+        temp.append(sympy.Symbol("b" + str(i)))
+        temp.append(sympy.Symbol("g" + str(i)))
+    
+    if not self.qaoa.options.get("append"):
+        self.circuit_param = temp
+    else:
+        self.qaoa.options['i0'] = np.size(self.circuit_param)
+        self.circuit_param.extend(temp)
 
+def _get_param_resolver(self, beta_values, gamma_values):
+    p = self.qaoa.options.get("p") 
+    assert p == np.size(beta_values), "Error: p = np.size(beta_values) required"
+    assert p == np.size(
+        gamma_values
+    ), "Error: p = np.size((self.circuit_param[1]) == len(gamma_values) required"
+    # order does not mater for python dictonary
+    if p == 1:
+        joined_dict = {**{"b0": beta_values}, **{"g0": gamma_values}}
+        # This is the same as:
+        # joined_dict = {**{"b" + str(i): beta_values for i in range(p)},\
+        #              **{"g" + str(i): gamma_values for i in range(p)}}
+    else:
+        joined_dict = {
+            **{"b" + str(i): beta_values[i] for i in range(p)},
+            **{"g" + str(i): gamma_values[i] for i in range(p)},
+        }
+    # Need return here, as yield does not work.
+    return cirq.ParamResolver(joined_dict)
+
+def _set_beta_values(self, beta_values):
+    p = self.qaoa.options.get("p")
+    i0 = self.qaoa.options.get("i0")
+
+    assert np.size(beta_values) == p, "Error: size(beta_values) !=  p; {} != {}".format(
+            np.size(beta_values), p
+        )
+
+    assert np.size(self.circuit_param_values) == 2 * p +i0, " np.size(self.circuit_param_values) !=  2p + i0; {} != {}".format(
+            np.size(self.circuit_param_values) , 2 * p +i0
+        )
+
+    if p > 1:
+        for i in range(p):
+            self.circuit_param_values[i0 + 2 * i] = beta_values[i]
+    else:
+        self.circuit_param_values[i0 + 0] = beta_values
+
+def _set_gamma_values(self, gamma_values):
+    p = self.qaoa.options.get("p")
+    i0 = self.qaoa.options.get("i0")
+
+    assert np.size(gamma_values) == p, "Error: size(gamma values) !=  p; {} != {}".format(
+            np.size(gamma_values), p
+        )
+
+    assert np.size(self.circuit_param_values) == 2 * p +i0, " np.size(self.circuit_param_values) !=  2p + i0; {} != {}".format(
+            np.size(self.circuit_param_values) , 2 * p +i0
+        )
+
+    if p > 1:
+        for i in range(p):
+            self.circuit_param_values[i0 + 2 * i + 1] = gamma_values[i]
+    else:
+        self.circuit_param_values[i0 + 1] = gamma_values
+      
 def _UB_layer(self, beta):
     """Generator for U(beta, B) layer (mixing layer) of QAOA"""
     for row in self.qubits:
         for qubit in row:
             yield cirq.X(qubit) ** beta
-
 
 def _UC_layer(self, gamma):
     """
@@ -210,23 +220,3 @@ def _Z_layer(self, gamma):
     for i in range(self.n[0]):
         for j in range(self.n[1]):
             yield cirq.Z(self.qubits[i][j]) ** (gamma * self.h[i, j][0])
-
-def _get_param_resolver(self, beta_values, gamma_values):
-    p = self.qaoa.options.get("p") 
-    assert p == np.size(beta_values), "Error: p = np.size(beta_values) required"
-    assert p == np.size(
-        gamma_values
-    ), "Error: p = np.size((self.circuit_param[1]) == len(gamma_values) required"
-    # order does not mater for python dictonary
-    if p == 1:
-        joined_dict = {**{"b0": beta_values}, **{"g0": gamma_values}}
-        # This is the same as:
-        # joined_dict = {**{"b" + str(i): beta_values for i in range(p)},\
-        #              **{"g" + str(i): gamma_values for i in range(p)}}
-    else:
-        joined_dict = {
-            **{"b" + str(i): beta_values[i] for i in range(p)},
-            **{"g" + str(i): gamma_values[i] for i in range(p)},
-        }
-    # Need return here, as yield does not work.
-    return cirq.ParamResolver(joined_dict)
