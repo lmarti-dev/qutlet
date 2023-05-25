ad0305e3e666ec268ce3ae5a14916dfd53f9d137
TimoEckstein <Timo.Eckstein@fau.de>
2023-05-25 14:29:57 +0200

    100 % Testcoverage GradientOptimiser

--- tests/test_optimiser_gradient_descent.py
+++ tests/test_optimiser_gradient_descent.py
@@ -15,12 +15,78 @@
 # external imports
 import pytest
 import numpy as np
+from typing import Optional
 
 import cirq
 
 # internal imports
 from fauvqe import Ising, GradientDescent, ExpectationValue, UtCost
 from fauvqe import IsingXY, AbstractExpectationValue, SpinModel
+
+class Mock_UtCost(UtCost):
+    def __init__(self, model, t, initial_wavefunctions ):
+        super().__init__(model, t, initial_wavefunctions=initial_wavefunctions)
+        delattr(self, '_time_steps')
+        self.evaluate = self._evaluate
+
+    def simulate( self, 
+                param_resolver = cirq.ParamResolver(), 
+                initial_state: Optional[np.ndarray] = None) -> np.ndarray:
+        if initial_state is None:
+            initial_state = self._initial_wavefunctions
+        
+        output_state = np.empty(shape=(1, *initial_state.shape), dtype = self._dtype)
+        
+        if len(initial_state) == self._N:
+            ini = initial_state
+        else:
+            ini = initial_state[0]
+
+        wf = self._model.simulator.simulate(
+                self._model.circuit,
+                param_resolver=param_resolver,
+                initial_state=ini,
+                ).state_vector()
+        output_state[0] = wf/np.linalg.norm(wf)
+
+        #Run through all states given in initial state
+        for i_state in range(1,round(np.size(initial_state)/self._N)):
+            wf = self._model.simulator.simulate(
+                self._model.circuit,
+                param_resolver=param_resolver,
+                initial_state=initial_state[i_state],
+                ).state_vector()
+            output_state[0, i_state] = wf/np.linalg.norm(wf)
+
+        return np.squeeze(output_state)
+    
+    def _evaluate( self,
+                       wavefunction: np.ndarray, 
+                       options: dict = {}) -> np.float64:
+        cost = 0
+        if options.get('state_indices') is None:
+            options['state_indices'] = range(np.size(self._output_wavefunctions[0,:,0]))
+
+        options['time_indices'] = range(1)
+        
+        if wavefunction.ndim == 1:
+            # This assumes a single state vector is given
+            return 1- abs(np.vdot(wavefunction, self._output_wavefunctions[0,0,:]) )**self._exponent
+        elif len(options.get('time_indices')) == 1 and wavefunction.ndim == 2:
+            #This covers the expected behaviour for using UTCost together with a batch of random vectors and 1 final simulation time
+            _tmp = [np.vdot(wavefunction[i], self._output_wavefunctions[0,i,:]) for i in range(np.size(wavefunction[:,0]))]
+            return 1- (abs(np.sum(_tmp))/len(wavefunction[:,0]))**self._exponent
+        else:
+        # Past comment: seems wrong? -> No test to that shows that?
+        # This fails UtCost self-consistency tests:
+         #This covers using UTCost together with a batch of more than one final simulation time
+         #Note here the inconsistent use of wavefunction compared to all other objects as
+         # dim 1: time steps dim 2: batc vector dim3: vector entries
+            for step in options.get('time_indices'):
+                cost += (1/len(options.get('state_indices')))*np.sum(1 - (abs(np.sum(np.conjugate(wavefunction[step])*
+                                            self._output_wavefunctions[step][options.get('state_indices')], 
+                                            axis=1)))**self._exponent )
+        return (1 /len(options.get('time_indices'))) * cost
 
 #This test misses a real assert
 @pytest.mark.higheffort
@@ -46,6 +112,7 @@ def test_set_optimiser():
 
 
 # This is potentially a higher effort test:
+# This covers line 223 in gradient descent but pytest does not show it...
 @pytest.mark.higheffort
 def test_optimise():
     ising_obj = Ising(
@@ -62,6 +129,7 @@ def test_optimise():
     gd = GradientDescent({
         'break_param':25,
         'eta':eta,
+        'use_progress_bar': True, 
     })
     obj = ExpectationValue(ising_obj)
     res = gd.optimise(obj)
@@ -72,7 +140,24 @@ def test_optimise():
     # Result smaller than -0.5 up to eta
 
 @pytest.mark.higheffort
-def test_optimise_batch():
+@pytest.mark.parametrize(
+    "cost_class, symmetric_gradient",
+    [
+        (
+            UtCost,
+            False,
+        ),
+        (
+            Mock_UtCost,
+            True,
+        ),
+        (
+            Mock_UtCost,
+            False,
+        ),
+    ]
+)
+def test_optimise_batch(cost_class, symmetric_gradient):
     t=0.5
     ising = Ising("GridQubit", [1, 4], np.ones((0, 4)), np.ones((1, 4)), np.ones((1,4)), "X", t)
     ising.set_Ut()
@@ -82,13 +167,13 @@ def test_optimise_batch():
     })
     ising.set_circuit_param_values(-(2/np.pi)*t/3 *np.ones(np.size(ising.circuit_param)))
     
-    bsize = 100
+    bsize = 1
     initial_rands= (np.random.rand(bsize, 16)).astype(np.complex128)
     initials = np.zeros(initial_rands.shape, dtype=np.complex64)
     for k in range(bsize):
         initials[k, :] = initial_rands[k, :] / np.linalg.norm(initial_rands[k, :])
     
-    objective = UtCost(ising, t, 0, initial_wavefunctions = initials)
+    objective = cost_class(ising, t, initial_wavefunctions = initials)
     
     wavefunction = objective.simulate(
         param_resolver=ising.get_param_resolver(ising.circuit_param_values), initial_state=initials[0]
@@ -96,13 +181,14 @@ def test_optimise_batch():
     trotter_cost = ( objective.evaluate(np.array([wavefunction]), options={'indices': [0]}) )
     print(trotter_cost)
     gd = GradientDescent({
-        'break_param': 100,
+        'break_param': 10,
         'batch_size': 1,
         'eps': 1e-5,
         'eta': 1e-2,
-        'symmetric_gradient': False
+        'symmetric_gradient': symmetric_gradient
     })
     print(objective.model.circuit_param_values.view())
+
     res = gd.optimise(objective)
     print(res.get_latest_step().params)
     wavefunction = objective.simulate(
