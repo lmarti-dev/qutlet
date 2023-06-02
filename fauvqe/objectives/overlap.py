cb58a7fee71548f6b38c7cbb49ed791d2b512956
TimoEckstein <Timo.Eckstein@fau.de>
2023-06-02 18:00:22 +0200

    Create Overlap objective

---
+++ fauvqe/objectives/overlap.py
@@ -0,0 +1,13 @@
+from numpy import float64 as np_float64
+from numpy import ndarray as np_ndarray
+
+from fauvqe.objectives.fidelity import Fidelity
+
+class Overlap(Fidelity):
+    def evaluate(self, 
+                 wavefunction: np_ndarray,
+                 target_state: np_ndarray = None) -> np_float64:
+        return np_sqrt(super().evaluate(wavefunction=wavefunction, target_state=target_state))
+    
+    def __repr__(self) -> str:
+        return "<Overlap target_state={}>".format(self._target_state)
