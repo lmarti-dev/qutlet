786b5903b9ea0fdda819bbe8217bcfb34c90aa63
TimoEckstein <Timo.Eckstein@fau.de>
2023-06-02 18:00:08 +0200

    Create Infidelity objective

---
+++ fauvqe/objectives/infidelity.py
@@ -0,0 +1,14 @@
+from numpy import float64 as np_float64
+from numpy import ndarray as np_ndarray
+
+from fauvqe.objectives.fidelity import Fidelity
+
+class Infidelity(Fidelity):
+    def evaluate(self, 
+                 wavefunction: np_ndarray,
+                 target_state: np_ndarray = None) -> np_float64:
+        return 1 - super().evaluate(wavefunction=wavefunction, target_state=target_state)
+    
+    def __repr__(self) -> str:
+        return "<Infidelity target_state={}>".format(self._target_state)
+    
