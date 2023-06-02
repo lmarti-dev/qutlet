3a1065a54cfc95e62df01870996b439dcec10fb1
TimoEckstein <Timo.Eckstein@fau.de>
2023-06-02 16:29:31 +0200

    Update utilities.visual

--- fauvqe/utilities/visual.py
+++ fauvqe/utilities/visual.py
@@ -2,44 +2,56 @@
     This Utilities submodule generates for visualisation
 
 """
+from __future__ import annotations
+
 #external imports
 from cirq import Heatmap as cirq_Heatmap
-import numpy as np
+
+from numpy import arange as np_arange
+from numpy import binary_repr as np_binary_repr
+from numpy import conj as np_conj
+from numpy import log2 as np_log2 
+from numpy import ndarray as np_ndarray
+from numpy import size as np_size
+from numpy import zeros as np_zeros
 
 #internal imports
-from fauvqe.models.abstractmodel import (
-    AbstractModel,
-)
+from typing import TYPE_CHECKING
+if TYPE_CHECKING: from fauvqe import AbstractModel 
+#This creats a cylc import together with the flatten import in AbstractModel
+#from fauvqe.models.abstractmodel import (
+#    AbstractModel
+#)
 
 
 def get_value_map_from_state(   model: AbstractModel, 
-                                state: np.array):
-    assert np.size(model.n) == 2, "Expect 2D qubit grid"
+                                state: np_ndarray):
+    assert np_size(model.n) == 2, "Expect 2D qubit grid"
     
     # probability from state
-    probability = abs(state * np.conj(state))
+    probability = abs(state * np_conj(state))
 
     # cumulative probability
-    _n = round(np.log2(state.shape[0]))
-    cumulative_probability = np.zeros(_n)
+    _n = round(np_log2(state.shape[0]))
+    cumulative_probability = np_zeros(_n)
 
     # Now sum it; this is a potential openmp sum; loop over com_prob, use index arrays to select correct
-    for i in np.arange(_n):
-        for j in np.arange(2 ** _n):
-            if np.binary_repr(j, width=_n)[i] == "1":
+    for i in np_arange(_n):
+        for j in np_arange(2 ** _n):
+            if np_binary_repr(j, width=_n)[i] == "1":
                 cumulative_probability[i] += probability[j]
     
     # This is for qubits:
-    # {(i1, i2): com_prob[i2 + i1*q4.n[1]] for i1 in np.arange(q4.n[0]) for i2 in np.arange(q4.n[1])}
+    # {(i1, i2): com_prob[i2 + i1*q4.n[1]] for i1 in np_arange(q4.n[0]) for i2 in np_arange(q4.n[1])}
     # But we want for spins:
     return {
         (model.qubits[i0][i1],): 2 * cumulative_probability[i1 + i0 * model.n[1]] - 1
-        for i0 in np.arange(model.n[0])
-        for i1 in np.arange(model.n[1])
+        for i0 in np_arange(model.n[0])
+        for i1 in np_arange(model.n[1])
         }
 
 def plot_heatmap(   model: AbstractModel, 
-                    state: np.array,):
+                    state: np_ndarray,):
     """
     Currently does not work due to Cirq update...
 
