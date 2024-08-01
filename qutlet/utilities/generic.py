fixed the default param handler issue with lists, allowed adapt to only compute the gradient concerned by tetris, and the match param val now has a return value, which should fix the issue of params not being updated in place. also fixed a typo Exciation to Excitation in gatepools

qutlet/utilities/generic.py
@@ -461,7 +461,7 @@ def default_value_handler(shape: tuple, value: Union[str, float, Iterable]):
         if not np.all(npvalue.shape == shape):
             raise ValueError(
                 "Given shape {s1} doesn't match given value shape {s2}".format(
-                    s1=shape, s2=value.shape
+                    s1=shape, s2=npvalue.shape
                 )
             )
         return value